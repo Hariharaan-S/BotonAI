861b15ae3e78c652f2d18c79cc21e98e11cb9977
Hariharaan S <s.hariharaan.college@gmail.com>
2023-09-18 01:09:27 +0530

    Modify app.py

--- Website/app.py
+++ Website/app.py
@@ -6,7 +6,6 @@ import tensorflow as tf
 from classification import classification 
 ALLOWED_EXTENSIONS = {'jpg'}
 
-# Load the model from the .h5 file
 model = tf.keras.models.load_model('D:\\SIH 2023\\Website\\keras_model.h5')
 
 supplier = {1: '<iframe title="supplier_1" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=8c7891eb-fbda-410d-b34c-3a4c10e9661c&autoAuth=true&ctid=02286583-175c-458f-98e0-234d4b040175" frameborder="0" allowFullScreen="true"></iframe>',2:'<iframe title="supplier_2" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=ddf84a0d-631c-4362-9bfc-eec22e9c3208&autoAuth=true&ctid=02286583-175c-458f-98e0-234d4b040175" frameborder="0" allowFullScreen="true"></iframe>'}
@@ -61,8 +60,6 @@ def predict():
     resized_image = cv2.resize(img, (224, 224))
     c = classification('D:\\SIH 2023\\Website\\keras_model.h5','D:\\SIH 2023\\Website\\labels.txt')
     prediction = c.classify_image(resized_image)   
-
-    # prediction = model.predict(img_batch)
     return prediction
 
 @app.route('/search')
@@ -76,12 +73,6 @@ def history():
 @app.route('/technician')
 def technician():
     return render_template("manufacture_identification.html")
-
-
-
-@app.route('/r')
-def r():
-    return render_template("report.html",supplier_report=supplier.get(1))
 
 @app.route('/report',methods=['GET','POST'])
 def report():
